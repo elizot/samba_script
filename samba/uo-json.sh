5e9d247750339f62b9a799ec15b542e2ba86730d
brai.niac <46333317+elizot@users.noreply.github.com>
2019-10-27 10:09:47 -0300

    script que gera o json da UO

---
+++ samba/uo-json.sh
@@ -0,0 +1,23 @@
+ #!/bin/bash
+ very=$(ldbsearch -H /var/lib/private/sam.ldb | grep "dc=COM" | grep "dn:" | sed -e 's/dn:    //g' -e 's/,/ /g'  | awk '{print $2}' | grep 'dc=' | awk 'NR==1 {print $1}')
+ array1=($(ldbsearch -H /var/lib/private/sam.ldb | grep "dc=COM" | grep "dn:" | sed -e 's/    dn://g' -e 's/,/ /g'  | awk '{print $1}'))
+ array2=($(ldbsearch -H /var/lib/private/sam.ldb | grep "dc=COM" | grep "dn:" | sed -e 's/    dn://g' -e 's/,/ /g'  | awk '{print $2}'))
+  
+ echo -e "[\n"
+ 
+ for i in $(seq 0 ${#array1[@]}); do
+     if [ "${array1[$i]}" = "" ]; then
+         break
+    elif [ $i -eq $((${#array1[@]}-1)) ]; then
+         if [ "${array2[$i]}" = "${very}" ]; then
+             echo -e "\t{\"ID\" : \"${array1[$i]}\", \"parent\" : \"#\"}" | sed 's/ou=//g'
+         else
+             echo -e "\t{\"ID\" : \"${array1[$i]}\", \"parent\" : \"${array2[$i]}\"}," | se    d 's/ou=//g'
+         fi
+     elif [ "${array2[$i]}" = "${very}" ]; then
+         echo -e "\t{\"ID\" : \"${array1[$i]}\", \"parent\" : \"#\"}," | sed 's/ou=//g'
+     else
+         echo -e "\t{\"ID\" : \"${array1[$i]}\", \"parent\" : \"${array2[$i]}\"}," | sed 's    /ou=//g'
+     fi
+done
+echo -e "]\n"
